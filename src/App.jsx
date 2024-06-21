7ef43df01dcf1bf7605c6443daba6ec3ca658ba4
EldarMemetov <eldarm9347@gmail.com>
2024-06-21 10:05:28 +0200

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import Options from './components/Options/Options';
 import Feedback from './components/Feedback/Feedback';
 import Notification from './components/Notification/Notification';
-
+import Description from './components/Description/Description';
 const App = () => {
   const [feedback, setFeedback] = useState(() => {
     const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
@@ -40,11 +40,7 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+      <Description />
       <Options
         updateFeedback={updateFeedback}
         resetFeedback={resetFeedback}
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,14 @@
+const Description = () => {
+  return (
+    <div>
+      <h2>About Sip Happens Café</h2>
+      <p>
+        Welcome to Sip Happens Café! We value your feedback and strive to
+        provide the best service possible. Please leave your feedback to help us
+        improve.
+      </p>
+    </div>
+  );
+};
+
+export default Description;
